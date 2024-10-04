0bee6c27a85ba0102030742daaacb791d9e6bbff
Chetan12bca <chetan12bca@gmail.com>
2024-10-04 07:02:54 +0530

    Add All Files

---
+++ src/Oct/ex_02102024/Lab01.java
@@ -0,0 +1,10 @@
+package Oct.ex_02102024;
+
+public class Lab01
+{
+    public  static void main(String[] args)
+        {
+            System.out.println("Hello World");
+            System.out.println("Namaste");
+        }
+}
