4d34c52da718fc6986f864a94cd32384b9328809
Chetan12bca <chetan12bca@gmail.com>
2024-10-04 07:15:05 +0530

    Add All Files

---
+++ src/Oct/ex_04102024/Addition.java
@@ -0,0 +1,13 @@
+package Oct.ex_04102024;
+
+public class Addition
+{
+    public static void main (String[] args)
+    {
+        int A=5, B=8, C;
+        {
+            C = A + B;
+            System.out.println("Sum is: " + C);
+        }
+    }
+}
