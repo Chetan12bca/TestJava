9fbc50e0d4e2a48aa01de12d1860a7c7ac56dace
Chetan12bca <chetan12bca@gmail.com>
2024-10-09 10:20:59 +0530

    Add All Files

---
+++ src/Oct/ex_08102024/Lab_007.java
@@ -0,0 +1,12 @@
+package Oct.ex_08102024;
+
+public class Lab_007
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        {
+            System.out.printf("%d * 1 = %d", a,a);
+        }
+    }
+}
