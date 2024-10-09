4d897a091ecf9c135e09912661a45df348634ddf
Chetan12bca <chetan12bca@gmail.com>
2024-10-09 09:04:55 +0530

    Add All Files

---
+++ src/Oct/Lab_varibales.java
@@ -0,0 +1,12 @@
+package Oct;
+
+public class Lab_varibales
+{
+    public static void main (String[] args)
+    {
+        int a = 10;
+        System.out.printf("%d * 1 = %d", a, a);
+        System.out.println();
+        System.out.printf("%d * 2 = %d", a, a);
+    }
+}
