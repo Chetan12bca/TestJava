1cb797de12fd493576f554d2e4f954896e70c1da
Chetan12bca <chetan12bca@gmail.com>
2024-10-15 08:55:07 +0530

    Add All Files

---
+++ src/Oct/ex_15102024/Lab_16.java
@@ -0,0 +1,11 @@
+package Oct.ex_15102024;
+
+public class Lab_16
+{
+    public static void main(String[] args)
+    {
+        int a=40, b=20, c=30;
+        int max = (a>b?a:b)>c?(a>b?a:b):c;
+        System.out.println(max);
+    }
+}
