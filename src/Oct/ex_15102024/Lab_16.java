0048080ab19c9ced413e1d879fcc2cbb6c0e755f
Chetan12bca <chetan12bca@gmail.com>
2024-10-16 07:20:20 +0530

    Add All Files

--- src/Oct/ex_15102024/Lab_16.java
+++ src/Oct/ex_15102024/Lab_16.java
@@ -4,8 +4,9 @@ public class Lab_16
 {
     public static void main(String[] args)
     {
-        int a=40, b=20, c=30;
-        int max = (a>b?a:b)>c?(a>b?a:b):c;
+        int a=40, b=50, c=30;
+        //int max = (a>b?a:b)>c?(a>b?a:b):c;
+        int max = (a >= b && a >= c) ? a : (b >= c ? b : c);
         System.out.println(max);
     }
 }
---
+++ src/Oct/ex_16102024/Lab_17_ScannerClass.java
@@ -0,0 +1,21 @@
+package Oct.ex_16102024;
+
+import java.util.Scanner;
+
+public class Lab_17_ScannerClass
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = sc.next();
+
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+
+        System.out.println("Name is:" + name + "\nAge is:"+age);
+
+        sc.close();
+    }
+}
